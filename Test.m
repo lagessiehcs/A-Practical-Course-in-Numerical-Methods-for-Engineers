ed97f26a08d613f32b9d7c094c939d6e8f23a1bb
quân <116118843+lagessiehcs@users.noreply.github.com>
2023-12-26 11:52:09 +0100

    Rename Fkt A to Fkt 0

--- Test.m
+++ Test.m
@@ -1,12 +1,11 @@
 clear 
 clc
-% format short
 addpath("Aufgabenblatt 1\")
-test_text_pass   = {'001:     Testing function: <Fkt A:        falcutaet(0)> ... passed (= 1, tol = 1e-12)!\n'
-                    '002:     Testing function: <Fkt A:        falcutaet(5)> ... passed (= 120, tol = 1e-12)!\n'};
+test_text_pass   = {'001:     Testing function: <Fkt 0:        falcutaet(0)> ... passed (= 1, tol = 1e-12)!\n'
+                    '002:     Testing function: <Fkt 0:        falcutaet(5)> ... passed (= 120, tol = 1e-12)!\n'};
 
-test_text_failed = {'001: !!! Testing function: <Fkt A:        falcutaet(0)> ... failed (1 ~= %f, diff = %f)\n';
-                    '002: !!! Testing function: <Fkt A:        falcutaet(5)> ... failed (1 ~= %f, diff = %f)\n'};
+test_text_failed = {'001: !!! Testing function: <Fkt 0:        falcutaet(0)> ... failed (1 ~= %f, diff = %f)\n';
+                    '002: !!! Testing function: <Fkt 0:        falcutaet(5)> ... failed (1 ~= %f, diff = %f)\n'};
 
               
 
