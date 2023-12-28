cdb8cdd0071f792a40e4ff1c66d82631902cc8e9
quân <116118843+lagessiehcs@users.noreply.github.com>
2023-12-28 12:50:23 +0100

    add solution for Aufgabe 1, Aufgabenblatt 6

---
+++ Aufgabenblatt 6/A1_Die_Familie_der_Einschritt_theta_Verfahren.m
@@ -0,0 +1,67 @@
+
+% ===========================================================
+% Allgemeine Form:
+% φn+1 = φn + θ ∆t f (tn+1, φn+1) + (1 − θ) ∆t f (tn, φn) (1)
+%
+% θ = 0:   das Vorwärts-Euler-Verfahren
+% θ = 1:   das Rückwärts-Euler-Verfahren
+% θ = 1/2: die Trapezregel
+% 
+% f (t,φ) = t^2*exp(−5t) − 6*φ 
+% ===========================================================
+
+% % close all
+clear
+clc
+
+phi = @(t) exp(-5.*t).*(t.^2-2.*t+2)-2.*exp(-6.*t); % exakte Lösung zum Vergleichen
+
+dt = [0.1 0.2 0.4];
+
+for k = 1:length(dt)
+
+    t = 0:dt(k):2;
+    
+    phi_vorwaerts   = zeros(1,length(t));
+    phi_rueckwaerts = zeros(1,length(t));
+    phi_trapez      = zeros(1,length(t));
+    
+    for i=1:length(t)-1
+        % Gl. (1) mit θ = 0:
+        phi_vorwaerts(i+1) = phi_vorwaerts(i) + ...
+                             dt(k) * (t(i)^2*exp(-5*t(i))-6*phi_vorwaerts(i));
+    
+        % Gl. (1) mit θ = 1 und lösen nach φn+1:
+        phi_rueckwaerts(i+1) = (phi_rueckwaerts(i) + ...
+                               dt(k) * t(i+1)^2*exp(-5*t(i+1)))/(1+6*dt(k));
+        
+        % Gl. (1) mit θ = 1/2 und lösen nach φn+1:
+        phi_trapez(i+1) = (...
+                           phi_trapez(i) + ...
+                           dt(k)/2 * ( t(i+1)^2 * exp(-5*t(i+1)) +      ...
+                           t(i)^2 * exp(-5*t(i)) - 6*phi_trapez(i) ) ...
+                           ) / (1+3*dt(k));
+    
+    end
+    
+    figure(k)
+    hold on
+    plot(t,phi_vorwaerts,'DisplayName','Vorwärts-Euler-Verfahren')
+    plot(t,phi_rueckwaerts,'DisplayName','Rückwärts-Euler-Verfahren')
+    plot(t,phi_trapez,'DisplayName','Trapezregel')
+    plot(0:0.01:2,phi(0:0.01:2),'k--','DisplayName','Exakt'); % exakte Lösung zum Vergleichen
+    legend
+    xlabel('t')
+    ylabel('\phi(t)')
+    title(['Lösung mit Zeitschrittlänge: ',num2str(dt(k))])
+    grid on
+
+end
+
+
+
+
+
+
+
+
