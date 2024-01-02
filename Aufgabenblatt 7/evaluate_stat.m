77333735f02feed52685f9406ddc2d20ba75a48c
quân <116118843+lagessiehcs@users.noreply.github.com>
2024-01-02 20:50:50 +0100

    correction: lamda -> lambda

--- Aufgabenblatt 7/evaluate_stat.m
+++ Aufgabenblatt 7/evaluate_stat.m
@@ -8,7 +8,7 @@ function [elemat,elevec] = evaluate_stat(elenodes,gpx,gpw)
 %
 % Rückgabewert: Elementmatrix A(e) und Elementvektor f(e)
 % =========================================================================
-lamda = 48;
+lambda = 48;
 
 elemat = zeros(4);
 
@@ -19,7 +19,7 @@ for k = 1:size(gpx,1)  % Schleife über alle Gaußpunkte im Element
     grad_N = linquadderivref(xi,eta)*invJ;
     for i = 1:size(elemat,1) % Schleife über alle Zeilen der Elementmatrix
         for j = 1:size(elemat,2) % Schleife über alle Spalten der Elementmatrix
-            elemat(i,j) = elemat(i,j) + lamda*grad_N(i,:)*grad_N(j,:)'*detJ*gpw(k);
+            elemat(i,j) = elemat(i,j) + lambda*grad_N(i,:)*grad_N(j,:)'*detJ*gpw(k);
         end
     end
 
